fe70c8a147da8e788e2c04c531b796d4de11b125
chrismatteson <chris.matteson@gmail.com>
2018-01-15 14:01:43 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,6 @@ provider "aws" {
 # Use a public keypair we specify
 resource "aws_key_pair" "public_key" {
     key_name   = "${var.projectName}-${var.stageName}-key"
-    public_key = "${file(var.publicSshKey)}"
+    public_key = "${var.publicSshKey}"
 }
 
